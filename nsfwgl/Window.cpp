1b4e0c8d1c69aed87d432ecbfd312d3c3f9461a1
Terry Nguyen <terreh@terrehbyte.com>
2015-10-17 21:53:07 -0700

    Implemented window.cpp

--- nsfwgl/Window.cpp
+++ nsfwgl/Window.cpp
@@ -1,52 +1,75 @@
 #include "Window.h"
 
 #include <glm\ext.hpp>
+#include <glCore\gl_core_4_4.h>
+#include <glfw\glfw3.h>
 
 #include "nsfw.h"
 
 void nsfw::Window::init(unsigned width, unsigned height)
 {
-	TODO_D("Should create and set an active windowing context. ONLY GLFW! No GL!");
+	//TODO_D("Should create and set an active windowing context. ONLY GLFW! No GL!");
+	assert(width > 0 && height > 0);	// validate window size
+
+	int status = 0;
+	status = glfwInit();
+	assert(status == GL_TRUE);	// glfw initialization
+
+	// @terrehbyte: consider the following... TODO_D("Expose a window title paramter.");
+	window = glfwCreateWindow(width, height, "NSFWGL - Not Suited for Work - Open GL Edition", nullptr, nullptr);
+
+	assert(window != nullptr);	// validate window created
+
+	glfwMakeContextCurrent(window);
+	
+	status = ogl_LoadFunctions();
+	assert(status == ogl_LOAD_SUCCEEDED);
+
+	return;
 }
 
 void nsfw::Window::step()
 {
-	TODO_D("GLFW poll events and swap buffers is all that should really be here! No GL!");
+	//TODO_D("GLFW poll events and swap buffers is all that should really be here! No GL!");
+	glfwPollEvents();
+	glfwSwapBuffers(window);
 }
 
 void nsfw::Window::term()
 {
-	TODO();
+	//TODO();
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
 
 float nsfw::Window::getTime() const
 {
-	TODO();
-	return 0.0f;
+	//TODO();
+	return glfwGetTime();
 }
 
 bool nsfw::Window::getKey(unsigned k) const
 {
-	TODO();
-	return false;
+	//TODO();
+	return glfwGetKey(window, k);
 }
 
 bool nsfw::Window::getShouldClose() const
 {
-	TODO();
-	return true;
+	//TODO();
+	return glfwWindowShouldClose(window);
 }
 
 unsigned nsfw::Window::getWidth() const
 {
-	TODO();
-	return 0;
+	//TODO();
+	return width;
 }
 
 unsigned nsfw::Window::getHeight() const
 {
-	TODO();
-	return 0;
+	//TODO();
+	return height;
 }
 
 glm::mat4 nsfw::Window::getTexelAdjustmentMatrix() const
